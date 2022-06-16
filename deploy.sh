2eec78f75ef581747c9cea0b35de52bf1dd53a42
Akshay Dubey <akshay.dubey055@gmail.com>
2022-06-16 20:07:44 +0530

    refactor: Add echo statements to deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 APPS="axon-server eureka-server api-gateway product-service order-service user-service payment-service"
-
+echo ">>>>> Stopping <<<<<"
 sh stop-all.sh
-
+echo ">>>>> Deploying <<<<<"
 for app in $APPS
 do
 	cd $app && sh run.sh
