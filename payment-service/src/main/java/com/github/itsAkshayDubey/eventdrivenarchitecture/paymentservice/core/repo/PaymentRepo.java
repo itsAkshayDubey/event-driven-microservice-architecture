30629306c83043027857c5a7e2e78f311025a917
Akshay Dubey <akshay.dubey055@gmail.com>
2022-06-17 23:39:06 +0530

    refactor: Replace payment-service with payments

--- payment-service/src/main/java/com/github/itsAkshayDubey/eventdrivenarchitecture/paymentservice/core/repo/PaymentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.github.itsAkshayDubey.eventdrivenarchitecture.paymentservice.core.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.github.itsAkshayDubey.eventdrivenarchitecture.paymentservice.core.entity.Payment;
-
-public interface PaymentRepo extends JpaRepository<Payment, String>{
-
-}
