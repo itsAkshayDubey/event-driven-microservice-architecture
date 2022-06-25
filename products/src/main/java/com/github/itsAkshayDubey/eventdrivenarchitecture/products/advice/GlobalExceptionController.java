171cc4e82f46ddc00ccf150266961768c73d8d1d
Akshay Dubey <akshay.dubey055@gmail.com>
2022-06-25 18:43:08 +0530

    feat: Add exception controller advice

---
+++ products/src/main/java/com/github/itsAkshayDubey/eventdrivenarchitecture/products/advice/GlobalExceptionController.java
@@ -0,0 +1,22 @@
+package com.github.itsAkshayDubey.eventdrivenarchitecture.products.advice;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.github.itsAkshayDubey.eventdrivenarchitecture.products.exception.ErrorDetails;
+
+@ControllerAdvice
+public class GlobalExceptionController {
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
